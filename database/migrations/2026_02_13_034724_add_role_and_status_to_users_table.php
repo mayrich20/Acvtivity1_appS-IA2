802f1a96e5a72bc98b75e79a6ff384b0f4c3518c
Mayrich <mayrichorit11@gmail.com>
2026-02-20 09:21:31 +0800

    Customize users table migration and model

--- database/migrations/2026_02_13_034724_add_role_and_status_to_users_table.php
+++ database/migrations/2026_02_13_034724_add_role_and_status_to_users_table.php
@@ -9,20 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+   public function up()
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->string('role')->default('user');
+        $table->boolean('is_active')->default(true);
+    });
+}
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+    public function down()
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn(['role', 'is_active']);
+    });
+}
 };
